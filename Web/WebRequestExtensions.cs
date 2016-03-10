26808471cae1fcba5dbdfa66b42cd5ac0d441515
joshcodes <josh@blackbarlabs.com>
2016-03-10 16:38:22 -0600

    added rest exteions

---
+++ Web/WebRequestExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Core.Web
+{
+    public static class WebRequestExtensions
+    {
+        public static async Task<TResult> PostAsync<TResource, TResult>(this WebRequest webRequest, TResource resource,
+            Func<HttpWebResponse, TResult> onSuccess, Func<HttpStatusCode, string, TResult> onFailure)
+        {
+            if (!(webRequest is HttpWebRequest))
+                throw new ArgumentException("webRequest must be of type HttpWebRequest");
+            var httpWebRequest = (HttpWebRequest)webRequest;
+
+            var resourceJson = Newtonsoft.Json.JsonConvert.SerializeObject(resource);
+
+            httpWebRequest.ContentType = "text/json";
+            httpWebRequest.Method = "POST";
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(resourceJson);
+                streamWriter.Flush();
+            }
+            try
+            {
+                var createAuthResponse = ((HttpWebResponse)(await httpWebRequest.GetResponseAsync()));
+                return onSuccess(createAuthResponse);
+            }
+            catch (WebException ex)
+            {
+                var httpResponse = (HttpWebResponse)ex.Response;
+                var responseText = new System.IO.StreamReader(httpResponse.GetResponseStream()).ReadToEnd();
+                return onFailure(httpResponse.StatusCode, responseText);
+            }
+        }
+    }
+}
