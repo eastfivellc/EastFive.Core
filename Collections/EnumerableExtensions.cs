03d1c8e5331e0db20aabc8716ca1c7026e184a44
joshcodes <josh@blackbarlabs.com>
2016-03-14 10:32:06 -0500

    added colleciotn and web extensions

---
+++ Collections/EnumerableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Collections.Generic
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> items, T item)
+        {
+            return items.Concat(new T[] { item });
+        }
+
+        public static IEnumerable<T> AppendYield<T>(this IEnumerable<T> items, Action<Action<T>> callback)
+        {
+            //foreach (var item in items)
+            //    yield return item;
+
+            var appendItems = new List<T>();
+            callback((item) =>
+            {
+                appendItems.Add(item);
+            });
+            return items.Concat(appendItems);
+        }
+    }
+}
