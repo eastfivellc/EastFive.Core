cda6bcb67a512cca41020cd972df21b39cda4a70
joshcodes <josh@blackbarlabs.com>
2016-03-25 12:30:51 -0500

    removed resource leaks in http comms

--- Web/WebRequestExtensions.cs
+++ Web/WebRequestExtensions.cs
@@ -10,19 +10,36 @@ namespace BlackBarLabs.Core.Web
 {
     public static class WebRequestExtensions
     {
-        public static async Task<TResult> PostAsync<TResource, TResult>(this WebRequest webRequest, TResource resource,
-            Func<HttpWebResponse, TResult> onSuccess, Func<HttpStatusCode, string, TResult> onFailure)
+        public static TResult GetRequest<TResult, TResource>(this HttpWebRequest httpWebRequest,
+            TResource resource,
+            Func<TResult> onSuccess, Func<WebException, TResult> onFailure)
         {
-            if (!(webRequest is HttpWebRequest))
-                throw new ArgumentException("webRequest must be of type HttpWebRequest");
-            var httpWebRequest = (HttpWebRequest)webRequest;
-            
-            httpWebRequest.Method = "POST";
             try
             {
-                httpWebRequest.Serialize(resource);
-                var createAuthResponse = ((HttpWebResponse)(await httpWebRequest.GetResponseAsync()));
-                return onSuccess(createAuthResponse);
+                httpWebRequest.ContentType = "application/json";
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    var resourceJson = Newtonsoft.Json.JsonConvert.SerializeObject(resource);
+                    streamWriter.Write(resourceJson);
+                    streamWriter.Flush();
+                }
+                return onSuccess();
+            }
+            catch (WebException ex)
+            {
+                return onFailure(ex);
+            }
+        }
+
+        public static async Task<TResult> GetResponseAsync<TResult, TResource>(this HttpWebRequest httpWebRequest,
+            Func<HttpWebResponse, TResult> onSuccess, Func<HttpStatusCode, string, TResult> onFailure)
+        {
+            try
+            {
+                using (var createAuthResponse = ((HttpWebResponse)(await httpWebRequest.GetResponseAsync())))
+                {
+                    return onSuccess(createAuthResponse);
+                }
             }
             catch (WebException ex)
             {
@@ -30,70 +47,61 @@ namespace BlackBarLabs.Core.Web
                 var responseText = new System.IO.StreamReader(httpResponse.GetResponseStream()).ReadToEnd();
                 return onFailure(httpResponse.StatusCode, responseText);
             }
+        }
+
+        public static HttpWebRequest AsHttpWebRequest(this WebRequest webRequest, string method)
+        {
+            if (!(webRequest is HttpWebRequest))
+                throw new ArgumentException("webRequest must be of type HttpWebRequest");
+            var httpWebRequest = (HttpWebRequest)webRequest;
+
+            httpWebRequest.Method = "POST";
+            return httpWebRequest;
+        }
+
+        public static Task<TResult> PostAsync<TResource, TResult>(this WebRequest webRequest, TResource resource,
+            Func<HttpWebResponse, TResult> onSuccess, Func<HttpStatusCode, string, TResult> onWebFailure, Func<string, TResult> onFailure)
+        {
+            var httpWebRequest = webRequest.AsHttpWebRequest("POST");
+            return httpWebRequest
+                .GetRequest<Task<TResult>, TResource>(resource,
+                    () => httpWebRequest.GetResponseAsync<TResult, TResource>(onSuccess, onWebFailure),
+                    (webEx) => Task.FromResult(onFailure(webEx.Message)));
         }
 
         public static async Task<TResult> GetAsync<TResource, TResult>(this WebRequest webRequest,
             Func<TResource, TResult> onSuccess, Func<HttpStatusCode, string, TResult> onFailure)
         {
-            if (!(webRequest is HttpWebRequest))
-                throw new ArgumentException("webRequest must be of type HttpWebRequest");
-            var httpWebRequest = (HttpWebRequest)webRequest;
-            
-            httpWebRequest.ContentType = "text/json";
-            httpWebRequest.Method = "GET";
-            try
-            {
-                var response = ((HttpWebResponse)(await httpWebRequest.GetResponseAsync()));
-                var responseJson = new StreamReader(response.GetResponseStream()).ReadToEnd();
-                var resource = Newtonsoft.Json.JsonConvert.DeserializeObject<TResource>(responseJson);
-                return onSuccess(resource);
-            }
-            catch (WebException ex)
-            {
-                var httpResponse = (HttpWebResponse)ex.Response;
-                var responseText = new System.IO.StreamReader(httpResponse.GetResponseStream()).ReadToEnd();
-                return onFailure(httpResponse.StatusCode, responseText);
-            }
-            catch (Exception ex)
-            {
-                return onFailure(HttpStatusCode.InternalServerError, ex.Message);
-            }
+            var httpWebRequest = webRequest.AsHttpWebRequest("GET");
+            httpWebRequest.ContentType = "application/json";
+            return await httpWebRequest.GetResponseAsync<TResult, TResource>(
+                (response) =>
+                {
+                    try
+                    {
+                        var responseJson = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                        var resource = Newtonsoft.Json.JsonConvert.DeserializeObject<TResource>(responseJson);
+                        return onSuccess(resource);
+                    }
+                    catch (Exception ex)
+                    {
+                        return onFailure(HttpStatusCode.InternalServerError, ex.Message);
+                    }
+                },
+                onFailure);
         }
 
-        private static void Serialize<TResource>(this HttpWebRequest httpWebRequest, TResource resource)
-        {
-            var resourceJson = Newtonsoft.Json.JsonConvert.SerializeObject(resource);
-
-            httpWebRequest.ContentType = "text/json";
-            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-            {
-                streamWriter.Write(resourceJson);
-                streamWriter.Flush();
-            }
-        }
-
-        public static async Task<TResult> DeleteAsync<TResource, TResult>(this WebRequest webRequest,
+        public static Task<TResult> DeleteAsync<TResource, TResult>(this WebRequest webRequest,
             TResource resource,
             Func<HttpWebResponse, TResult> onSuccess,
-            Func<HttpStatusCode, string, TResult> onFailure)
+            Func<HttpStatusCode, string, TResult> onWebFailure,
+            Func<string, TResult> onFailure)
         {
-            if (!(webRequest is HttpWebRequest))
-                throw new ArgumentException("webRequest must be of type HttpWebRequest");
-            var httpWebRequest = (HttpWebRequest)webRequest;
-
-            httpWebRequest.Method = "DELETE";
-            try
-            {
-                httpWebRequest.Serialize(resource);
-                var response = ((HttpWebResponse)(await httpWebRequest.GetResponseAsync()));
-                return onSuccess(response);
-            }
-            catch (WebException ex)
-            {
-                var httpResponse = (HttpWebResponse)ex.Response;
-                var responseText = new System.IO.StreamReader(httpResponse.GetResponseStream()).ReadToEnd();
-                return onFailure(httpResponse.StatusCode, responseText);
-            }
+            var httpWebRequest = webRequest.AsHttpWebRequest("DELETE");
+            return httpWebRequest
+                .GetRequest<Task<TResult>, TResource>(resource,
+                    () => httpWebRequest.GetResponseAsync<TResult, TResource>(onSuccess, onWebFailure),
+                    (webEx) => Task.FromResult(onFailure(webEx.Message)));
         }
     }
 }
