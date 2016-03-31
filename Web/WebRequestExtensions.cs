414117c884568725feb24230491362f61b01919f
joshcodes <josh@blackbarlabs.com>
2016-03-31 12:51:04 -0500

    refactored web requests to have more accure naming, more consistent convention and fixed a few bugs in web requests

--- Web/WebRequestExtensions.cs
+++ Web/WebRequestExtensions.cs
@@ -10,29 +10,44 @@ namespace BlackBarLabs.Core.Web
 {
     public static class WebRequestExtensions
     {
-        public static TResult GetRequest<TResult, TResource>(this HttpWebRequest httpWebRequest,
-            TResource resource,
-            Func<TResult> onSuccess, Func<WebException, TResult> onFailure)
+        public static HttpWebRequest AsHttpWebRequest(this WebRequest webRequest, string method)
+        {
+            if (!(webRequest is HttpWebRequest))
+                throw new ArgumentException("webRequest must be of type HttpWebRequest");
+            var httpWebRequest = (HttpWebRequest)webRequest;
+
+            httpWebRequest.Method = method;
+            return httpWebRequest;
+        }
+
+        #region resource safe access to request and response
+
+        public static TResult GetRequestStream<TResult>(this HttpWebRequest httpWebRequest,
+            Func<Stream, TResult> success,
+            Func<HttpStatusCode, string, TResult> webFailure,
+            Func<string, TResult> failure)
         {
             try
             {
-                httpWebRequest.ContentType = "application/json";
-                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                using (var requestStream = httpWebRequest.GetRequestStream())
                 {
-                    var resourceJson = Newtonsoft.Json.JsonConvert.SerializeObject(resource);
-                    streamWriter.Write(resourceJson);
-                    streamWriter.Flush();
+                    return success(requestStream);
                 }
-                return onSuccess();
             }
             catch (WebException ex)
             {
-                return onFailure(ex);
+                var httpResponse = (HttpWebResponse)ex.Response;
+                if (default(HttpWebResponse) == httpResponse)
+                    return failure(ex.Message);
+                var responseText = new System.IO.StreamReader(httpResponse.GetResponseStream()).ReadToEnd();
+                return webFailure(httpResponse.StatusCode, responseText);
             }
         }
 
-        public static async Task<TResult> GetResponseAsync<TResult, TResource>(this HttpWebRequest httpWebRequest,
-            Func<HttpWebResponse, TResult> onSuccess, Func<HttpStatusCode, string, TResult> onFailure)
+        public static async Task<TResult> GetResponseAsync<TResult>(this HttpWebRequest httpWebRequest,
+            Func<HttpWebResponse, TResult> onSuccess,
+            Func<HttpStatusCode, string, TResult> webFailure,
+            Func<string, TResult> failure)
         {
             try
             {
@@ -44,37 +59,52 @@ namespace BlackBarLabs.Core.Web
             catch (WebException ex)
             {
                 var httpResponse = (HttpWebResponse)ex.Response;
+                if (default(HttpWebResponse) == httpResponse)
+                    return failure(ex.Message);
                 var responseText = new System.IO.StreamReader(httpResponse.GetResponseStream()).ReadToEnd();
-                return onFailure(httpResponse.StatusCode, responseText);
+                return webFailure(httpResponse.StatusCode, responseText);
             }
         }
 
-        public static HttpWebRequest AsHttpWebRequest(this WebRequest webRequest, string method)
-        {
-            if (!(webRequest is HttpWebRequest))
-                throw new ArgumentException("webRequest must be of type HttpWebRequest");
-            var httpWebRequest = (HttpWebRequest)webRequest;
+        #endregion
 
-            httpWebRequest.Method = "POST";
-            return httpWebRequest;
-        }
+        #region JSON Parsing
 
-        public static Task<TResult> PostAsync<TResource, TResult>(this WebRequest webRequest, TResource resource,
-            Func<HttpWebResponse, TResult> onSuccess, Func<HttpStatusCode, string, TResult> onWebFailure, Func<string, TResult> onFailure)
+        public static TResult GetRequestJson<TResult, TResource>(this HttpWebRequest httpWebRequest,
+            TResource resource,
+            Func<TResult> success,
+            Func<HttpStatusCode, string, TResult> webFailure,
+            Func<string, TResult> failure)
         {
-            var httpWebRequest = webRequest.AsHttpWebRequest("POST");
-            return httpWebRequest
-                .GetRequest<Task<TResult>, TResource>(resource,
-                    () => httpWebRequest.GetResponseAsync<TResult, TResource>(onSuccess, onWebFailure),
-                    (webEx) => Task.FromResult(onFailure(webEx.Message)));
-        }
-
-        public static async Task<TResult> GetAsync<TResource, TResult>(this WebRequest webRequest,
-            Func<TResource, TResult> onSuccess, Func<HttpStatusCode, string, TResult> onFailure)
-        {
-            var httpWebRequest = webRequest.AsHttpWebRequest("GET");
             httpWebRequest.ContentType = "application/json";
-            return await httpWebRequest.GetResponseAsync<TResult, TResource>(
+            return httpWebRequest.GetRequestStream(
+                (requestStream) =>
+                {
+                    try
+                    {
+                        using (var streamWriter = new StreamWriter(requestStream))
+                        {
+                            var resourceJson = Newtonsoft.Json.JsonConvert.SerializeObject(resource);
+                            streamWriter.Write(resourceJson);
+                            streamWriter.Flush();
+                        }
+                        return success();
+                    }
+                    catch (Exception ex)
+                    {
+                        return failure(ex.Message);
+                    }
+                },
+                webFailure,
+                failure);
+        }
+
+        public static async Task<TResult> GetResponseJsonAsync<TResource, TResult>(this HttpWebRequest httpWebRequest,
+            Func<TResource, TResult> onSuccess,
+            Func<HttpStatusCode, string, TResult> webFailure,
+            Func<string, TResult> failure)
+        {
+            return await httpWebRequest.GetResponseAsync(
                 (response) =>
                 {
                     try
@@ -85,10 +115,40 @@ namespace BlackBarLabs.Core.Web
                     }
                     catch (Exception ex)
                     {
-                        return onFailure(HttpStatusCode.InternalServerError, ex.Message);
+                        return failure(ex.Message);
                     }
                 },
-                onFailure);
+                webFailure, failure);
+        }
+
+        #endregion
+
+        #region methods
+
+        public static async Task<TResult> GetAsync<TResource, TResult>(this WebRequest webRequest,
+            Func<TResource, TResult> onSuccess,
+            Func<HttpStatusCode, string, TResult> webFailure,
+            Func<string, TResult> failure)
+        {
+            var httpWebRequest = webRequest.AsHttpWebRequest("GET");
+            httpWebRequest.ContentType = "application/json";
+            return await httpWebRequest.GetResponseJsonAsync<TResource, TResult>(
+                (response) => onSuccess(response),
+                webFailure,
+                failure);
+        }
+
+        public static Task<TResult> PostAsync<TResource, TResult>(this WebRequest webRequest, TResource resource,
+            Func<HttpWebResponse, TResult> onSuccess,
+            Func<HttpStatusCode, string, TResult> onWebFailure,
+            Func<string, TResult> onFailure)
+        {
+            var httpWebRequest = webRequest.AsHttpWebRequest("POST");
+            return httpWebRequest
+                .GetRequestJson(resource,
+                    () => httpWebRequest.GetResponseAsync(onSuccess, onWebFailure, onFailure),
+                    (code, message) => Task.FromResult(onWebFailure(code, message)),
+                    (whyFailed) => Task.FromResult(onFailure(whyFailed)));
         }
 
         public static Task<TResult> DeleteAsync<TResource, TResult>(this WebRequest webRequest,
@@ -99,9 +159,13 @@ namespace BlackBarLabs.Core.Web
         {
             var httpWebRequest = webRequest.AsHttpWebRequest("DELETE");
             return httpWebRequest
-                .GetRequest<Task<TResult>, TResource>(resource,
-                    () => httpWebRequest.GetResponseAsync<TResult, TResource>(onSuccess, onWebFailure),
-                    (webEx) => Task.FromResult(onFailure(webEx.Message)));
+                .GetRequestJson(resource,
+                    () => httpWebRequest.GetResponseAsync(onSuccess, onWebFailure, onFailure),
+                    (code, message) => Task.FromResult(onWebFailure(code, message)),
+                    (whyFailed) => Task.FromResult(onFailure(whyFailed)));
         }
+
+        #endregion
+
     }
 }
