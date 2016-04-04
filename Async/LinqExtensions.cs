fade40513db6be723a08a86d4824096b8e4db105
joshcodes <josh@blackbarlabs.com>
2016-04-04 17:15:33 -0500

    added more ToEnumerable overrides

--- Async/LinqExtensions.cs
+++ Async/LinqExtensions.cs
@@ -97,7 +97,15 @@ namespace BlackBarLabs.Collections.Async
             return iterator;
         }
 
+        public static IEnumerable<TResult> ToEnumerable<TDelegateItems, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(
+            this IEnumerableAsync<TDelegateItems> items,
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult> convert)
+        {
+            var iterator = items.GetEnumerable<TResult, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>>(convert);
+            return iterator;
+        }
+
         #endregion
-        
+
     }
 }
