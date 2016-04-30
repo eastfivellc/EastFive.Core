a74279676cb730de6f7905565db18e60784c6701
Keith Holloway <keith@eastfive.com>
2016-04-29 20:35:11 -0500

    Added another, even longer ToEnumerable

--- Async/LinqExtensions.cs
+++ Async/LinqExtensions.cs
@@ -256,6 +256,14 @@ namespace BlackBarLabs.Collections.Async
             return iterator;
         }
 
+        public static IEnumerable<TResult> ToEnumerable<TDelegateItems, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(
+            this IEnumerableAsync<TDelegateItems> items,
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult> convert)
+        {
+            var iterator = items.GetEnumerable<TResult, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>>(convert);
+            return iterator;
+        }
+
         #endregion
 
     }
