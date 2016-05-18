03a537e1a743f2f031f86126d3f81ef6b4a7f402
Jeremy Randolph <randocodes@gmail.com>
2016-05-17 20:01:47 -0500

    Updating Linq

--- Async/LinqExtensions.cs
+++ Async/LinqExtensions.cs
@@ -288,6 +288,14 @@ namespace BlackBarLabs.Collections.Async
             return iterator;
         }
 
+        public static IEnumerable<TResult> ToEnumerable<TDelegateItems, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>(
+            this IEnumerableAsync<TDelegateItems> items,
+            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult> convert)
+        {
+            var iterator = items.GetEnumerable<TResult, Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, TResult>>(convert);
+            return iterator;
+        }
+
         public static IEnumerable<TResult> ToEnumerable<TDelegateItems, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(
            this IEnumerableAsync<TDelegateItems> items,
            Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult> convert)
