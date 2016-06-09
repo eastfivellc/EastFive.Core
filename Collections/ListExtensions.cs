dff97ec82a8d17b7b7956d9334e9579308b9eec4
Keith Holloway <keith@eastfive.com>
2016-06-09 10:27:17 -0500

    Added ListExtensions

---
+++ Collections/ListExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BlackBarLabs.Core.Collections
+{
+    public static class ListExtensions
+    {
+        public static void AddIfNotExisting<TValue>(this IList<TValue> list, TValue value)
+        {
+            if (!list.Contains(value))
+            {
+                list.Add(value);
+            }
+        }
+    }
+}
