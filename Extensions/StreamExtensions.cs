268c26f8ac3ba931fe21c8113ba5c245ee5cf925
Josh <josh@eastfive.com>
2016-09-13 18:44:13 -0500

    added stream extensions

---
+++ Extensions/StreamExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Core
+{
+    public static class StreamExtensions
+    {
+        public static TResult ToBytes<TResult>(this Stream stream,
+            Func<byte [], TResult> success)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                var bytes = memoryStream.ToArray();
+                return success(bytes);
+            }
+        }
+    }
+}
