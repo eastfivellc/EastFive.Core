e0348cd4cb596262dfc51bbe6e29e8f6dad58d63
Josh <josh@eastfive.com>
2016-09-16 10:14:56 -0500

    added tyep extensions

---
+++ Extensions/TypeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Core
+{
+    public static class TypeExtensions
+    {
+        public static T[] GetCustomAttributes<T>(this Type type, bool inherit = false)
+            where T : class
+        {
+            var attributes = type.GetCustomAttributes(typeof(T), inherit);
+            return attributes.Select(attrib => attrib as T).ToArray();
+        }
+    }
+}
