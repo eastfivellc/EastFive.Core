8148b695db98b7882f2b9ae63f5c2f01dc4a4d79
Josh <josh@eastfive.com>
2016-09-21 08:41:54 -0500

    Added new URI extension for ToUri()

---
+++ Extensions/UriExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Core
+{
+    public static class UriExtensions
+    {
+        public static Uri ToUri(this string uriString)
+        {
+            return new Uri(uriString);
+        }
+
+        public static bool AreEqual(this Uri uri1, Uri uri2,
+            UriComponents partsToCompare = UriComponents.AbsoluteUri,
+            UriFormat compareFormat = UriFormat.SafeUnescaped,
+            StringComparison comparisonType = StringComparison.OrdinalIgnoreCase)
+        {
+            return Uri.Compare(uri1, uri2, partsToCompare, compareFormat, comparisonType) == 0;
+        }
+
+        public static bool AreEqual(this Uri uri1, string uri2String,
+            UriComponents partsToCompare = UriComponents.AbsoluteUri,
+            UriFormat compareFormat = UriFormat.SafeUnescaped,
+            StringComparison comparisonType = StringComparison.OrdinalIgnoreCase)
+        {
+            var uri2 = new Uri(uri2String);
+            return uri1.AreEqual(uri2);
+        }
+    }
+}
