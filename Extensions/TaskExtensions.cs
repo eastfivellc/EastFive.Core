d70b34f343a5f27f063d789b9a5bc08ec69b9fd0
Josh <josh@eastfive.com>
2016-10-07 15:32:08 -0500

    WhenAllAsync can now have boundry parallel parameter

--- Extensions/TaskExtensions.cs
+++ Extensions/TaskExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlackBarLabs.Collections.Generic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,17 @@ namespace BlackBarLabs.Core
 {
     public static class TaskExtensions
     {
-        public static Task<T[]> WhenAllAsync<T>(this IEnumerable<Task<T>> tasks)
+        public static async Task<T[]> WhenAllAsync<T>(this IEnumerable<Task<T>> tasks, int parallelLimit = 0)
         {
-            return Task.WhenAll(tasks);
+            if(parallelLimit <= 0)
+                return await Task.WhenAll(tasks);
+            IEnumerable<T> results = new T[] { };
+            foreach (var task in tasks)
+            {
+                var item = await task;
+                results = results.Append(item);
+            }
+            return results.ToArray();
         }
     }
 }
