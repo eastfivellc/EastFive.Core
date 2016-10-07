753fdbbb302524310c2ba958760b725f3f01d17b
Josh <josh@eastfive.com>
2016-10-07 13:45:20 -0500

    added WhennAllAsync enumerable extension

---
+++ Extensions/TaskExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Core
+{
+    public static class TaskExtensions
+    {
+        public static Task<T[]> WhenAllAsync<T>(this IEnumerable<Task<T>> tasks)
+        {
+            return Task.WhenAll(tasks);
+        }
+    }
+}
