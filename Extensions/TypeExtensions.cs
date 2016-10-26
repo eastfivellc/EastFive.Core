caa867260ef4dc032b70a5b77e7de142b01fde26
Josh <josh@eastfive.com>
2016-10-26 10:49:02 -0500

    moved type casting into core extension

--- Extensions/TypeExtensions.cs
+++ Extensions/TypeExtensions.cs
@@ -21,5 +21,26 @@ namespace BlackBarLabs.Core
             var attributes = type.GetCustomAttributes<T>(inherit);
             return attributes.Count() > 0;
         }
+
+        public static object ConvertObject(this Type type, object obj)
+        {
+            if (type == typeof(string))
+                return obj.ToString();
+            if (type == typeof(short))
+                return Convert.ToInt16(obj);
+            if (type == typeof(int))
+                return Convert.ToInt32(obj);
+            if (type == typeof(long))
+                return Convert.ToInt64(obj);
+            if (type == typeof(bool))
+                return Convert.ToBoolean(obj);
+            if (type == typeof(decimal))
+                return Convert.ToDecimal(obj);
+            if (type == typeof(double))
+                return Convert.ToDouble(obj);
+            if (type == typeof(float))
+                return Convert.ToDecimal(obj);
+            throw new ArgumentException("Unsupported conversion type:" + type.FullName);
+        }
     }
 }
