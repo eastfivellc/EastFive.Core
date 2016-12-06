8b5372e814580b9dd8b6071b11dc5fc3a6509aab
Josh <josh@eastfive.com>
2016-12-06 16:03:54 -0600

    change AddIfNotExisting to be more functional

--- Collections/ListExtensions.cs
+++ Collections/ListExtensions.cs
@@ -1,15 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace BlackBarLabs.Core.Collections
 {
     public static class ListExtensions
     {
-        public static void AddIfNotExisting<TValue>(this IList<TValue> list, TValue value)
+        public static IEnumerable<TValue> AddIfNotExisting<TValue>(this IEnumerable<TValue> items, TValue value)
+            where TValue : IComparable
         {
-            if (!list.Contains(value))
+            var found = false;
+            foreach(var item in items)
             {
-                list.Add(value);
+                if (!found && item.CompareTo(value) == 0)
+                    found = true;
+                yield return item;
             }
+            if (!found)
+                yield return value;
         }
     }
 }
