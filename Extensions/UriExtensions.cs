d8dca9ed79410648af9eccda07340bea5c9c75b7
Josh <josh@eastfive.com>
2017-01-18 17:38:13 -0600

    added support methods for query strings on URLs

--- Extensions/UriExtensions.cs
+++ Extensions/UriExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
 namespace BlackBarLabs
 {
@@ -24,6 +27,28 @@ namespace BlackBarLabs
         {
             var uri2 = new Uri(uri2String);
             return uri1.AreEqual(uri2);
+        }
+
+        public static string ToQueryString(this IDictionary<string, string> source)
+        {
+            // From: http://stackoverflow.com/questions/829080/how-to-build-a-query-string-for-a-url-in-c#5158497
+            return String.Join("&", source.Select(kvp => String.Format("{0}={1}", HttpUtility.UrlEncode(kvp.Key), HttpUtility.UrlEncode(kvp.Value))).ToArray());
+        }
+
+        public static Uri AddQuery(this Uri uri, string name, string value)
+        {
+            // From: http://stackoverflow.com/questions/829080/how-to-build-a-query-string-for-a-url-in-c#20492373
+            
+            // this actually returns HttpValueCollection : NameValueCollection
+            // which uses unicode compliant encoding on ToString()
+            var query = HttpUtility.ParseQueryString(uri.Query);
+            query.Add(name, value);
+            var uriBuilder = new UriBuilder(uri)
+            {
+                Query = query.ToString()
+            };
+
+            return uriBuilder.Uri;
         }
     }
 }
