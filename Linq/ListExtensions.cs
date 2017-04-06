dcede8e4eb105dea9427e6e72ba48b026d83e8a1
Keith Holloway <keith@eastfive.com>
2017-04-06 09:27:16 -0500

    Added override of AddIfNotExisting to allow concat of two enumerables

--- Linq/ListExtensions.cs
+++ Linq/ListExtensions.cs
@@ -18,5 +18,22 @@ namespace BlackBarLabs.Linq
             if (!found)
                 yield return value;
         }
+
+        public static IEnumerable<TValue> AddIfNotExisting<TValue>(this IEnumerable<TValue> items, TValue[] values)
+            where TValue : IComparable
+        {
+            foreach (var value in values)
+            {
+                var found = false;
+                foreach (var item in items)
+                {
+                    if (!found && item.CompareTo(value) == 0)
+                        found = true;
+                    yield return item;
+                }
+                if (!found)
+                    yield return value;
+            }
+        }
     }
 }
