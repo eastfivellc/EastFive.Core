2bfe2c93fc45826090fdd38717a745ecedb161b4
Josh <josh@eastfive.com>
2017-08-10 13:24:47 -0500

    added regex extensions

---
+++ Extensions/RegexExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs
+{
+    public static class RegexExtensions
+    {
+        public static IEnumerable<Match> AsMatches(this MatchCollection collection)
+        {
+            foreach (Match match in collection)
+                yield return match;
+        }
+    }
+}
