4f627c33352108f0ec61c33452a654e4c7e385f8
Josh <josh@eastfive.com>
2017-08-30 12:27:37 -0500

    added string to ExceptionFunction extension

---
+++ Extensions/TResultExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs
+{
+    public static class TResultExtensions
+    {
+        public static Func<TResult> AsFunctionException<TResult>(this string message)
+        {
+            throw new Exception(message);
+        }
+
+        public static Func<T1, TResult> AsFunctionException<T1, TResult>(this string message)
+        {
+            throw new Exception(message);
+        }
+    }
+}
