e1f13061faf6f4c5d42e2c907a90097f653d70ac
Josh <josh@eastfive.com>
2017-09-15 11:02:06 -0500

    added Recursive dictionary

---
+++ Structs/RecursiveDictionary.cs
@@ -0,0 +1,30 @@
+﻿using BlackBarLabs.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBarLabs.Collections.Generic
+{
+    public class RecursiveDictionary<TKey> : Dictionary<TKey, RecursiveDictionary<TKey>>
+    {
+    }
+
+    public static class RecursiveDictionaryExtensions
+    {
+
+        public static IEnumerable<T> Flatten<T>(this RecursiveDictionary<T> dictionary)
+        {
+            return dictionary
+                .SelectMany(
+                    kvp =>
+                    {
+                        if (default(RecursiveDictionary<T>) == kvp.Value)
+                            return new T[] { kvp.Key };
+                        return dictionary.Flatten().Append(kvp.Key);
+                    });
+        }
+    }
+
+}
