801c0eaa388233fc973bb32c1d238806e5fe9ae2
Scott Turner <turner-scott@tds.net>
2017-12-28 19:31:07 -0600

    make a way for enums to be pretty printed

---
+++ Extensions/EnumExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace BlackBarLabs.Core.Extensions
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// When an enum value has a Description attribute, it will be returned rather than the constant name.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string GetDescriptionOrName(this Enum value)
+        {
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+            if (null != name)
+            {
+                FieldInfo field = type.GetField(name);
+                if (null != field)
+                {
+                    if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attr)
+                        return attr.Description;
+                }
+            }
+            return name ?? string.Empty;
+        }
+    }
+}
