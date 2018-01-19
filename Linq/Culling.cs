e4aa8ffffafdc86310d938bcae7eddad00148120
Josh Wingstrom <josh@eastfive.com>
2018-01-19 06:09:15 -0600

    added If* linqs

---
+++ Linq/Culling.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Linq
+{
+    public static class Culling
+    {
+        public static IEnumerable<TItem> If<TItem>(this IEnumerable<TItem> items,
+            bool condition,
+            Func<IEnumerable<TItem>, IEnumerable<TItem>> ifTrue)
+        {
+            if (condition)
+                return ifTrue(items);
+            return items;
+        }
+
+        public static IEnumerable<TItem> If<TItem>(this IEnumerable<TItem> items,
+            bool condition,
+            Func<IEnumerable<TItem>, IEnumerable<TItem>> ifTrue,
+            Func<IEnumerable<TItem>, IEnumerable<TItem>> ifNotTrue)
+        {
+            if (condition)
+                return ifTrue(items);
+            return ifNotTrue(items);
+        }
+
+        public static IEnumerable<TItem> IfWhere<TItem>(this IEnumerable<TItem> items,
+            bool condition,
+            Func<TItem, bool> predicate)
+        {
+            return items.If(condition, 
+                itemsIfTrue => itemsIfTrue.Where(predicate));
+        }
+    }
+}
