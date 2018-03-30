fb3765f324a349022088126fac99bfd888c56d88
Josh Wingstrom <josh@eastfive.com>
2018-03-29 21:43:42 -0500

    made ReduceItems less prone to stack overflow

--- Linq/ReduceExtensions.cs
+++ Linq/ReduceExtensions.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EastFive.Linq
@@ -146,7 +147,7 @@ namespace EastFive.Linq
             return callback(items.Current,
                 () => items.Reduce(callback, onEmpty));
         }
-
+        
         public static IEnumerable<TResult> ReduceItems<T1, TItem, TResult>(this IEnumerable<TItem> items,
             T1 v1,
             Func<
@@ -160,7 +161,110 @@ namespace EastFive.Linq
                 (v1_, items_) => items_);
         }
 
+        private struct ReduceItemStep<TSelection>
+        {
+            public ManualResetEvent trigger;
+            public TSelection selection;
+        }
+
+        public static TResult ReduceItems<TItem, TSelect, TResult>(this IEnumerable<TItem> items,
+            Func<
+                TItem,
+                Func<TSelect, TResult>,  // next
+                Func<TResult>, // skip
+                TResult> callback,
+            Func<IEnumerable<TSelect>, TResult> complete)
+        {
+            var block = new ManualResetEvent(false);
+            TResult result = default(TResult);
+
+            var steps = items
+                .Select(
+                    item =>
+                    {
+                        var step = new ReduceItemStep<TSelect>
+                        {
+                            trigger = new ManualResetEvent(false),
+                        };
+                        callback(
+                            item,
+                            (selection) =>
+                            {
+                                step.selection = selection;
+                                step.trigger.Set();
+                                block.WaitOne();
+                                return result;
+                            },
+                            () =>
+                            {
+                                step.trigger.Set();
+                                block.WaitOne();
+                                return result;
+                            });
+                        return step;
+                    })
+                .Select(step =>
+                    {
+                        step.trigger.WaitOne();
+                        return step.selection;
+                    });
+            result = complete(steps);
+            block.Set();
+            return result;
+        }
+
         public static TResult ReduceItems<T1, TItem, TSelect, TResult>(this IEnumerable<TItem> items,
+            T1 v1,
+            Func<
+                T1, TItem,
+                Func<TSelect, T1, TResult>,  // next
+                Func<T1, TResult>, // skip
+                TResult> callback,
+            Func<T1, IEnumerable<TSelect>, TResult> complete)
+        {
+            TResult result = default(TResult);
+            var block = new ManualResetEvent(false);
+
+            return items
+                .Aggregate(
+                    v1.PairWithValue(new TSelect[] { }),
+                    (valueKvp, item) =>
+                    {
+                        T1 vNext = default(T1);
+                        var selected = valueKvp.Value;
+                        var trigger = new ManualResetEvent(false);
+                        var task = new Task(
+                            () => callback(
+                                valueKvp.Key,
+                                item,
+                                (selection, valueNext) =>
+                                {
+                                    vNext = valueNext;
+                                    selected = selected.Append(selection).ToArray();
+                                    trigger.Set();
+                                    block.WaitOne();
+                                    return result;
+                                },
+                                (valueNext) =>
+                                {
+                                    vNext = valueNext;
+                                    trigger.Set();
+                                    block.WaitOne();
+                                    return result;
+                                }));
+                        task.Start();
+                        trigger.WaitOne();
+                        return vNext.PairWithValue(selected);
+                    },
+                    final =>
+                    {
+                        result = complete(final.Key, final.Value);
+                        block.Set();
+                        return result;
+                    });
+        }
+
+        public static TResult ReduceItemsX<T1, TItem, TSelect, TResult>(this IEnumerable<TItem> items,
             T1 v1,
             Func<
                 T1, TItem,
@@ -184,6 +288,8 @@ namespace EastFive.Linq
         {
             if (!items.MoveNext())
                 return complete(v1, new TSelect[] { });
+
+
 
             return callback(v1, items.Current,
                 (r, v1next) => items.ReduceItems(v1next, callback, (v1_, items_) => complete(v1_, items_.Append(r))),
