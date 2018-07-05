c7fde3a0964d43945a0564b8ab6c4222f065345d
Josh Wingstrom <josh@eastfive.com>
2018-07-05 16:19:11 -0500

    added nullable extension

---
+++ Extensions/NullableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using BlackBarLabs.Collections.Generic;
+using BlackBarLabs.Linq;
+using EastFive.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlackBarLabs.Extensions;
+using EastFive.Linq;
+
+namespace EastFive
+{
+    public static class NullableExtensions
+    {
+        public static TResult GetValueOrDefault<T, TResult>(this Nullable<T> valueMaybe,
+            Func<T, TResult> onValue,
+            TResult defaultValue)
+            where T : struct
+        {
+            if (!valueMaybe.HasValue)
+                return defaultValue;
+            var result = onValue(valueMaybe.Value);
+            return result;
+        }
+        
+    }
+}
