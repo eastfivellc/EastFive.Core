9e03ad1a245b228775c295aa4729f3392006333b
Josh Wingstrom <josh@eastfive.com>
2018-07-06 12:05:46 -0500

    improvements for MapReduce with Tail

--- Linq/Reduce/MapReduceExtensions.cs
+++ Linq/Reduce/MapReduceExtensions.cs
@@ -275,49 +275,75 @@ namespace EastFive.Linq
                 Task<TResult>> callback,
             Func<TSelect[], T1, Task<TResult>> complete)
         {
-            var globalSelection = new TSelect[] { };
+            var globalSelection = new List<TSelect>();
             var itemsEnumerator = items.GetEnumerator();
-            var nextTask = default(Task<TResult>);
+            //var lastTask = default(Task<TResult>);
             while (true)
             {
                 if(!itemsEnumerator.MoveNext())
-                    return await complete(globalSelection, item1);
+                    return await complete(globalSelection.ToArray(), item1);
                 var item = itemsEnumerator.Current;
 
-                var block = new ManualResetEvent(false);
+                //var block = new ManualResetEvent(false);
                 var tailed = false;
-                nextTask = callback(
+                var tailedValue = default(TResult).ToTask();
+                var lastTask = callback(
                     item, item1,
                     (selection, item1Next) =>
                     {
-                        globalSelection = globalSelection.Append(selection).ToArray();
+                        globalSelection.Add(selection);
+                        //globalSelection = globalSelection.Add(selection).ToArray();
                         item1 = item1Next;
-                        block.Set();
-                        return nextTask;
+                        //block.Set();
+                        return tailedValue; // lastTask;
                     },
                     (item1Next) =>
                     {
                         item1 = item1Next;
-                        block.Set();
-                        return nextTask;
+                        //block.Set();
+                        return tailedValue; // lastTask;
                     },
                     (tailResult) =>
                     {
                         tailed = true;
-                        block.Set();
+                        //block.Set();
+                        tailedValue = tailResult;
                         return tailResult;
                     });
-                block.WaitOne(); // wait here so Item1 is updated
-                if (tailed)
-                    return await nextTask;
 
-                nextTask = await nextTask.ContinueWith(
-                    (lastTaskInner) =>
-                    {
-                        return nextTask;
-                    });
+
+                var taskChainNext = (object)lastTask;
+                while (true)
+                {
+                    if (taskChainNext.IsDefaultOrNull())
+                        break;
+
+                    var taskChainNextType = taskChainNext.GetType();
+                    if (!taskChainNextType.IsGenericType)
+                        break;
+                    if (taskChainNextType.GetGenericTypeDefinition() != typeof(Task<>))
+                        break;
+
+                    var task = (Task)taskChainNext;
+                    await task.ConfigureAwait(false);
+                    // Harvest the result
+                    taskChainNext = (object)((dynamic)task).Result;
+                }
+
+                if (tailed)
+                    return await tailedValue;
             }
         }
+
+        //private async Task<TResult> BuildContinuation<TResult>(Task<TResult> taskToContinue, Func<Task<TResult>> getNextTask)
+        //{
+        //    return await await taskToContinue.ContinueWith(
+        //        (lastTaskInner) =>
+        //        {
+        //            return getNextTask();
+        //        });
+
+        //}
 
         /// <summary>
         /// This method is tail optimized mean that calling methods should not use the return values from callback's next/skip
