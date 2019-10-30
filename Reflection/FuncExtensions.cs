7338cf855a0986018ae82d53f08632b87f19af52
Joshua Wingstrom <josh@eastfive.com>
2019-10-30 05:44:44 -0500

    new function extensions

---
+++ Reflection/FuncExtensions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Reflection
+{
+    public static class FuncExtensions
+    {
+        public static object MakeDelegate<TResult>(this Func<TResult> func, Type delegateType)
+        {
+            var delegateGeneric = Delegate.CreateDelegate(delegateType,
+                func.Target, func.Method);
+            return delegateGeneric;
+        }
+
+        public static object MakeDelegate<T1, TResult>(this Func<T1, TResult> func, Type delegateType)
+        {
+            var delegateGeneric = Delegate.CreateDelegate(delegateType,
+                func.Target, func.Method);
+            return delegateGeneric;
+        }
+
+        public static object MakeDelegate<T1, T2, TResult>(this Func<T1, T2, TResult> func, Type delegateType)
+        {
+            var delegateGeneric = Delegate.CreateDelegate(delegateType,
+                func.Target, func.Method);
+            return delegateGeneric;
+        }
+
+        public static object MakeDelegate<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> func, Type delegateType)
+        {
+            var delegateGeneric = Delegate.CreateDelegate(delegateType,
+                func.Target, func.Method);
+            return delegateGeneric;
+        }
+
+        public static object MakeDelegate<T1, T2, T3, T4, TResult>(this Func<T1, T2, T3, T4, TResult> func, Type delegateType)
+        {
+            var delegateGeneric = Delegate.CreateDelegate(delegateType,
+                func.Target, func.Method);
+            return delegateGeneric;
+        }
+
+        public static object MakeDelegate<T1, T2, T3, T4, T5, TResult>(this Func<T1, T2, T3, T4, T5, TResult> func, Type delegateType)
+        {
+            var delegateGeneric = Delegate.CreateDelegate(delegateType,
+                func.Target, func.Method);
+            return delegateGeneric;
+        }
+
+        public static object MakeDelegate<T1, T2, T3, T4, T5, T6, TResult>(this Func<T1, T2, T3, T4, T5, T6, TResult> func, Type delegateType)
+        {
+            var delegateGeneric = Delegate.CreateDelegate(delegateType,
+                func.Target, func.Method);
+            return delegateGeneric;
+        }
+
+        public static TDelegate MakeDelegate<TDelegate, TResult>(this Func<TResult> func)
+            where TDelegate : MulticastDelegate
+        {
+            var delegateGeneric = Delegate.CreateDelegate(typeof(TDelegate),
+                func.Target, func.Method);
+            var delegateCast = delegateGeneric as TDelegate;
+            return delegateCast;
+        }
+    }
+}
