456956ad524e8eb4f13c79825c2ab68019f858a3
Joshua Wingstrom <josh@eastfive.com>
2020-01-06 13:31:19 -0600

    added Composible queries

---
+++ Linq/Expressions/IComposibleQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Linq.Expressions
+{
+    public interface IComposibleQuery<T>
+    {
+        IQueryable<T> FromExpression(Expression condition);
+    }
+}
