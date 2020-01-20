90ca3ebe18b1cdf9fd60d29ab3e48074e6bbfa46
Joshua Wingstrom <josh@eastfive.com>
2020-01-20 08:50:57 -0600

    added more capability to expression parsing

---
+++ Reflection/Assignment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Reflection
+{
+    public struct Assignment
+    {
+        public MemberInfo member;
+        public ExpressionType type;
+        public object value;
+    }
+}
