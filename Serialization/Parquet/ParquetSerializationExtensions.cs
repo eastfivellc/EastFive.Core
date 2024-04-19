6fe657b618bad7ed6314e10c325bbcbd7202c824
Joshua Wingstrom <josh@eastfive.com>
2024-04-19 12:25:50 -0500

    Added ability to write parquet data out to stream

--- Serialization/Parquet/ParquetSerializationExtensions.cs
+++ Serialization/Parquet/ParquetSerializationExtensions.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-
+using EastFive.Extensions;
 using EastFive.Linq;
 using EastFive.Linq.Async;
+using Parquet;
+using Parquet.Data;
 
 namespace EastFive.Serialization.Parquet
 {
@@ -33,6 +35,42 @@ namespace EastFive.Serialization.Parquet
                         var msg = $"{typeof(TResource).FullName} does not have an attribute implementing {nameof(IMapParquet)}.";
                         throw new Exception(msg);
                     });
+        }
+
+        public static void WriteToParquetStream(this IEnumerable<(string name, Type type, object value)[]> rows, Schema schema, Stream stream)
+        {
+            using (var writer = new ParquetWriter(schema, stream))
+            {
+                var table = rows
+                    .Aggregate(
+                        new global::Parquet.Data.Rows.Table(schema),
+                        (table, row) =>
+                        {
+                            var values = row
+                                .Select(
+                                    col =>
+                                    {
+                                        if (col.value.IsNull())
+                                            return col.value;
+
+                                        if (col.value.GetType() == typeof(System.DBNull))
+                                            return null;
+
+                                        if (col.value.GetType() == typeof(DateTime))
+                                            return new DateTimeOffset(((DateTime)col.value));
+
+                                        //if (col.type == typeof(DateTime))
+                                        //    if (col.value.GetType() == typeof(DateTimeOffset))
+                                        //        return ((DateTimeOffset)col.value).DateTime;
+
+                                        return col.value;
+                                    });
+                            var parquetRow = new global::Parquet.Data.Rows.Row(values);
+                            table.Add(parquetRow);
+                            return table;
+                        });
+                writer.Write(table);
+            }
         }
 	
 	}
